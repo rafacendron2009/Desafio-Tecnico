706adfda36886ba4f57116822211fed80f2ed089
AMCOM\rafael.cendron <rafael.cendron@amcom.com.br>
2025-01-03 16:13:48 -0300

    Add

---
+++ Tarefas.Repository.Imp/Context/TarefasContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarefas.Entities.Entities;
+
+namespace Tarefas.Repository.Imp.Context
+{
+    public class TarefasContext : DbContext
+    {
+        public TarefasContext(DbContextOptions<TarefasContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=localhost.db",
+                options => options.MigrationsAssembly("Tarefas.Api"));  // Aponta para o assembly correto
+        }
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+    }
+}
---
+++ Tarefas.Repository.Imp/Repositories/TarefaRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarefas.Entities.Entities;
+using Tarefas.Repository.Imp.Context;
+using Tarefas.Repository.IRepository;
+
+namespace Tarefas.Repository.Imp.Repositories
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private readonly TarefasContext _context;
+
+        public TarefaRepository(TarefasContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Tarefa>> GetAllAsync() =>
+            await _context.Tarefas.ToListAsync();
+
+        public async Task<Tarefa> GetByIdAsync(int id) =>
+            await _context.Tarefas.FindAsync(id);
+
+        public async Task AddAsync(Tarefa task)
+        {
+            _context.Tarefas.Add(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Tarefa task)
+        {
+            _context.Tarefas.Update(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var task = await GetByIdAsync(id);
+            if (task != null)
+            {
+                _context.Tarefas.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
