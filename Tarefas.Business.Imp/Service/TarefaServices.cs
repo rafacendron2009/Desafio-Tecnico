693ddc41dc269ad96060bd5f9e86ca9589ee8529
AMCOM\rafael.cendron <rafael.cendron@amcom.com.br>
2025-01-03 16:16:13 -0300

    Add

---
+++ Tarefas.Business.Imp/Service/TarefaServices.cs
@@ -0,0 +1,31 @@
+﻿using Tarefas.Business.IService;
+using Tarefas.Entities.Entities;
+using Tarefas.Repository.IRepository;
+
+namespace Tarefas.Business.Imp.Service
+{
+    public class TarefaServices : ITarefaServices
+    {
+        private readonly ITarefaRepository _repository;
+
+        public TarefaServices(ITarefaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Tarefa>> GetTarefaAsync() =>
+            await _repository.GetAllAsync();
+
+        public async Task<Tarefa> GetTarefaAsync(int id) =>
+            await _repository.GetByIdAsync(id);
+
+        public async Task CreateTarefaAsync(Tarefa task) =>
+            await _repository.AddAsync(task);
+
+        public async Task UpdateTarefaAsync(Tarefa task) =>
+            await _repository.UpdateAsync(task);
+
+        public async Task DeleteTarefaAsync(int id) =>
+            await _repository.DeleteAsync(id);
+    }
+}
---
+++ Tarefas.Business/IService/ITarefaServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarefas.Entities.Entities;
+
+namespace Tarefas.Business.IService
+{
+    public interface ITarefaServices
+    {
+        Task<IEnumerable<Tarefa>> GetTarefaAsync();
+        Task<Tarefa> GetTarefaAsync(int id);
+        Task CreateTarefaAsync(Tarefa task);
+        Task UpdateTarefaAsync(Tarefa task);
+        Task DeleteTarefaAsync(int id);
+    }
+}
